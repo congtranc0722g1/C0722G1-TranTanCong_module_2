36464756aa4d5b2a18c99ccdf09c0e57505aab94
congtranc0722g1 <trantancongall@gmail.com>
2022-09-05 14:21:55 +0700

    hi

--- module2_jame/src/ss1_introduction_to_java/thuc_hanh/HelloWorld.java
+++ module2_jame/src/ss1_introduction_to_java/thuc_hanh/HelloWorld.java
@@ -2,6 +2,6 @@ package ss1_introduction_to_java.thuc_hanh;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world 1");
     }
 }

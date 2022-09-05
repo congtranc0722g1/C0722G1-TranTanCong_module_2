e227d8ff5547261445cd9ca58e385337bf1267a1
congtranc0722g1 <trantancongall@gmail.com>
2022-09-05 17:07:22 +0700

    nop

---
+++ module2_jame/src/ss1_introduction_to_java/practise/HelloWorld.java
@@ -0,0 +1,7 @@
+package ss1_introduction_to_java.practise;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world cong");
+    }
+}
--- module2_jame/src/ss1_introduction_to_java/thuc_hanh/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ss1_introduction_to_java.thuc_hanh;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hello world 1");
-    }
-}

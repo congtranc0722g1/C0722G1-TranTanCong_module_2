4ccd271d8abf8162a63545f383b40e4468508fbd
congtranc0722g1 <trantancongall@gmail.com>
2022-09-07 11:18:33 +0700

    hi

--- module2_jame/src/ss2_loop/exercise/ShowPrimesLessThan100.java
+++ module2_jame/src/ss2_loop/exercise/ShowPrimesLessThan100.java
@@ -1,20 +1,17 @@
 package ss2_loop.exercise;
 
-public class ShowFirst20Primes {
+public class ShowPrimesLessThan100 {
     public static void main(String[] args) {
         int N = 2;
-        int times = 0;
-        while (times < 20){
+        while (N < 100){
             int count = 0;
             for (int i = 1; i <= N; i++) {
                 if (N % i == 0){
                     count++;
                 }
-
             }
             if(count == 2){
                 System.out.print(N + "\t");
-                times++;
             }
             N++;
         }

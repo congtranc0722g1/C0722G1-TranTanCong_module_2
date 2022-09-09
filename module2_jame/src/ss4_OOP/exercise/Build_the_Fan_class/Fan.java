8af60e20d5193d02056aa6aa648dbcd8cc521977
congtranc0722g1 <trantancongall@gmail.com>
2022-09-09 17:14:25 +0700

    nop

--- module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/Fan.java
+++ module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/Fan.java
@@ -4,4 +4,11 @@ public class Fan {
     final byte SLOW = 1;
     final byte MEDIUM = 2;
     final byte FAST = 3;
+    private int speed = SLOW;
+    private boolean status = false;
+    private double radius = 5;
+    private String color = "blue";
+    public Fan(){
+
+    }
 }

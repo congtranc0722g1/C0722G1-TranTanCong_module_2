920ca22de32afc56acbae10901ea155e45c7b88e
congtranc0722g1 <trantancongall@gmail.com>
2022-09-09 19:35:03 +0700

    nop

--- module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/Fan.java
+++ module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/Fan.java
@@ -1,14 +1,33 @@
 package ss4_OOP.exercise.Build_the_Fan_class;
 
 public class Fan {
-    final byte SLOW = 1;
-    final byte MEDIUM = 2;
-    final byte FAST = 3;
-    private int speed = SLOW;
-    private boolean status = false;
-    private double radius = 5;
-    private String color = "blue";
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+    int speed = SLOW;
+    boolean status = false;
+    double radius = 5;
+    String color = "blue";
     public Fan(){
 
+    }
+    public Fan(int speed, boolean status, double radius,String color){
+        this.speed = speed;
+        this.status = status;
+        this.radius = radius;
+        this.color = color;
+    }
+    public int getSpeed() {
+
+        return this.speed;
+    }
+    public boolean getStatus() {
+        return this.status;
+    }
+    public double getRadius() {
+        return this.radius;
+    }
+    public String getColor() {
+        return this.color;
     }
 }
--- module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/main.java
+++ module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/main.java
@@ -1,4 +1,10 @@
 package ss4_OOP.exercise.Build_the_Fan_class;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(2, true, 10, "red");
+        System.out.print("Trạng thái: " + fan1.getStatus() + "\nTốc độ: " + fan1.getSpeed() + "\nBán kính : " + fan1.getRadius() + "\nMàu sắc: " + fan1.getColor());
+    }
 }

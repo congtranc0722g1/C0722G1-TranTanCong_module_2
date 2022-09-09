11c9a4b5745fe1c1faa77aacbd37fab052724a97
congtranc0722g1 <trantancongall@gmail.com>
2022-09-09 17:05:02 +0700

    nop

---
+++ module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/Fan.java
@@ -0,0 +1,7 @@
+package ss4_OOP.exercise.Build_the_Fan_class;
+
+public class Fan {
+    final byte SLOW = 1;
+    final byte MEDIUM = 2;
+    final byte FAST = 3;
+}
---
+++ module2_jame/src/ss4_OOP/exercise/Build_the_Fan_class/main.java
@@ -0,0 +1,4 @@
+package ss4_OOP.exercise.Build_the_Fan_class;
+
+public class main {
+}

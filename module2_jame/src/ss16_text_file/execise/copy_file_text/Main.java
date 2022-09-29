ca3c27637a5891aba521596ad84c611659952ada
congtranc0722g1 <trantancongall@gmail.com>
2022-09-29 20:23:39 +0700

    nop

--- module2_jame/src/ss16_text_file/execise/copy_file_text/Main.java
+++ module2_jame/src/ss16_text_file/execise/copy_file_text/Main.java
@@ -4,22 +4,33 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("src\\ss16_text_file\\execise\\copy_file_text\\source.txt");
-        FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        try {
+            File file = new File("src\\ss16_text_file\\execise\\copy_file_text\\source.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        File file1 = new File("src\\ss16_text_file\\execise\\copy_file_text\\targe.txt");
-        FileWriter fileWriter = new FileWriter(file1);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            File file1 = new File("src\\ss16_text_file\\execise\\copy_file_text\\targe.txt");
+            FileWriter fileWriter = new FileWriter(file1);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
 
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            bufferedWriter.write(line);
-            bufferedWriter.newLine();
+            String line;
+            int count = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) != ' ') {
+                        count += 1;
+                    }
+                }
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+            bufferedReader.close();
+            System.out.println("Số ký tự trong file là: " + count);
+        }catch (FileNotFoundException e){
+            System.out.println("Nguồn không tồn tại");
         }
-        bufferedWriter.close();
-        bufferedReader.close();
 
 
 
